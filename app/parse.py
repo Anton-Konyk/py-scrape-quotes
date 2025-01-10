18343a87448b405c751875c3db05d2404e05449c
Anton Konyk <antonkonyk.com>
2025-01-10 21:04:08 +0100

    create parse_single_product

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from bs4 import BeautifulSoup, Tag
 
 
 @dataclass
@@ -6,6 +7,14 @@ class Quote:
     text: str
     author: str
     tags: list[str]
+
+
+def parse_single_product(quote: Tag) -> Quote:
+    return Quote(
+        text=quote.select_one(".text").text,  # text
+        author=quote.select_one(".author").text,
+        tags=quote.select_one(".keywords")["content"].split(","),
+    )
 
 
 def main(output_csv_path: str) -> None:
