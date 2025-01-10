055f27d3d4ebc064e291a1458c11f8a530392041
Anton Konyk <antonkonyk.com>
2025-01-10 21:07:16 +0100

    create get_all_page_quotes

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,12 @@
 from dataclasses import dataclass
+from dataclasses import dataclass, fields, astuple
+from urllib.parse import urljoin
+
+import requests
 from bs4 import BeautifulSoup, Tag
+
+
+BASE_URL = "https://quotes.toscrape.com/"
 
 
 @dataclass
@@ -22,8 +29,24 @@ def get_single_page_quotes(pages_soup: Tag) -> [Quote]:
     return [parse_single_product(product) for product in quote]
 
 
+def get_all_page_quotes() -> [Quote]:
+    logging.info("Starting parsing first page")
+    text = requests.get(BASE_URL).content
+    first_page_soup = BeautifulSoup(text, "html.parser")
+    all_quotes = get_single_page_quotes(first_page_soup)
+    next_page = first_page_soup.select("li.next a")
+    while next_page:
+        logging.info(f"Starting parsing first page {next_page[0]["href"].strip('/').split('/')[1]}")
+        next_page_url = urljoin(BASE_URL, next_page[0]["href"])
+        text = requests.get(next_page_url).content
+        next_page_soup = BeautifulSoup(text, "html.parser")
+        all_quotes.extend(get_single_page_quotes(next_page_soup))
+        next_page = next_page_soup.select("li.next a")
+
+    return all_quotes
+
 def main(output_csv_path: str) -> None:
-    pass
+    write_quotes_to_csv(output_csv_path, get_all_page_quotes())
 
 
 if __name__ == "__main__":
